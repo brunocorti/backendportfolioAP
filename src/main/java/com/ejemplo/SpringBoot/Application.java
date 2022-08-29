5a6fde473ed09e6c1add86f7627b71841bd20ce7
brunocorti <104029918+brunocorti@users.noreply.github.com>
2022-08-29 15:01:05 -0300

    corregi cors

--- src/main/java/com/ejemplo/SpringBoot/Application.java
+++ src/main/java/com/ejemplo/SpringBoot/Application.java
@@ -18,7 +18,7 @@ public class Application {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+                registry.addMapping("/**").allowedOrigins("https://portfolioap-4f438.web.app/");
             }
         };
     }
